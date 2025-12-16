fb7e25c6726b452098d79afb4c3ae126115ac588
sunxiaohui19891003-design <sunxiaohui19891003@gmail.com>
2025-12-16 10:35:17 +0900

    add favorite feature

--- src/main/java/com/example/miniweibo/controller/FavoriteController.java
+++ src/main/java/com/example/miniweibo/controller/FavoriteController.java
@@ -1,12 +1,15 @@
 package com.example.miniweibo.controller;
 
 import com.example.miniweibo.entity.Favorite;
+import com.example.miniweibo.entity.Weibo;
 import com.example.miniweibo.service.FavoriteService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -17,5 +20,10 @@ public class FavoriteController {
     public boolean addFavorite (HttpSession httpSession, Long weiboId){
         Long userId = (Long)httpSession.getAttribute("userId");
         return favoriteService.addFavorite(userId,weiboId);
+    }
+    @GetMapping("/favorites")
+    public List<Weibo> zhaoFavorites(HttpSession httpSession,Long weiboId){
+        Long userId = (Long)httpSession.getAttribute("userId");
+        return favoriteService.zhaoFavorites(userId,weiboId);
     }
 }
--- src/main/java/com/example/miniweibo/service/FavoriteService.java
+++ src/main/java/com/example/miniweibo/service/FavoriteService.java
@@ -1,16 +1,22 @@
 package com.example.miniweibo.service;
 
 import com.example.miniweibo.entity.Favorite;
+import com.example.miniweibo.entity.Weibo;
 import com.example.miniweibo.repository.FavoriteRepository;
+import com.example.miniweibo.repository.WeiboRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class FavoriteService {
     @Autowired
     FavoriteRepository favoriteRepository;
+    @Autowired
+    WeiboRepository weiboRepository;
     public boolean addFavorite (Long userId,Long weiboId){
         Optional<Favorite> count = favoriteRepository.findByUserIdAndWeiboId(userId,weiboId);
         if(count.isPresent()){
@@ -23,5 +29,14 @@ public class FavoriteService {
             favoriteRepository.save(favorite);
         }
         return true;
+    }
+
+    public List<Weibo> zhaoFavorites(Long userId, Long weiboId) {
+        List<Favorite> zhaoFavorites = favoriteRepository.findByUserId(userId);
+        List<Long> weiboIds = new ArrayList<>();
+        for(Favorite f : zhaoFavorites){
+            weiboIds.add(f.getWeiboId());
+        }
+        return weiboRepository.findAllById(weiboIds);
     }
 }
